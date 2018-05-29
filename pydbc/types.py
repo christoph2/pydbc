c0bc6c5abe03701b6f32e213eb218ddb56534c83
Christoph Schueler <cpu12.gems@googlemail.com>
2018-05-29 10:27:48 +0200

    Add types.py

---
+++ pydbc/types.py
@@ -0,0 +1,48 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__copyright__ = """
+   pySART - Simplified AUTOSAR-Toolkit for Python.
+
+   (C) 2010-2018 by Christoph Schueler <cpu12.gems.googlemail.com>
+
+   All Rights Reserved
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+   s. FLOSS-EXCEPTION.txt
+"""
+__author__  = 'Christoph Schueler'
+__version__ = '0.1.0'
+
+
+import enum
+
+class AttributeType(enum.IntEnum):
+    NODE = 0
+    MESSAGE = 1
+    SIGNAL = 2
+    ENV_VAR = 3
+    GENERAL = 4
+
+
+class ValueType(enum.IntEnum):
+    INT = 0
+    HEX = 1
+    FLOAT = 2
+    STRING = 3
+    ENUM = 4
+
