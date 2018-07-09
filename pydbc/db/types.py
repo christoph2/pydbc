5ee9baac527d9efe22ae5b45a16b7ffbb353f3a1
Christoph Schueler <cpu12.gems@googlemail.com>
2018-07-09 17:07:49 +0200

    Add address types.

--- pydbc/db/types.py
+++ pydbc/db/types.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -46,3 +45,64 @@ class ValueType(enum.IntEnum):
     STRING = 3
     ENUM = 4
 
+
+class IdentifierType(enum.IntEnum):
+    STANDARD = 0
+    EXTENDED = 1
+
+EXTENDED_ID_MASK = 0x80000000
+
+
+class AddressBaseType:
+
+    def __init__(self):
+        pass
+
+
+class CANAddress(AddressBaseType):
+    """
+    """
+
+    def __init__(self, rawId):
+        if (rawId & EXTENDED_ID_MASK) == EXTENDED_ID_MASK:
+            self.mtype = IdentifierType.EXTENDED
+            self.mid = rawId & (~0x80000000)
+        else:
+            self.mtype = IdentifierType.STANDARD
+            self.mid = mid = rawId
+
+    def __str__(self):
+        """
+        """
+        return "{}({:08x} [{}])".format(self.__class__.__name__, self.mid, self.mtype.name)
+
+    def __int__(self):
+        """
+        """
+        value = self.mid
+        if self.mtype == IdentifierType.EXTENDED:
+            value |= EXTENDED_ID_MASK
+        return value
+
+
+    __repr__ = __str__
+
+
+class J1939Address(AddressBaseType):
+
+    def __init__(self, rawId):
+        # TODO: check for extID
+        rawId &= (~0x80000000)
+        self.priority = (rawId & 0x1c000000) >> 26
+        self.reserved = (rawId & 0x2000000) >> 25
+        self.datapage = (rawId & 0x1000000) >> 24
+        self.pduFormat = (rawId & 0xff0000) >> 16
+        self.pduSpecific = (rawId & 0xff00) >> 8
+        self.sourceAddress = rawId & 0xff
+
+    def __str__(self):
+        return "{}(prio = {} r = {} dp = {} pf = {:02x} ps = {:02x} sa = {:02x} )".format(self.__class__.__name__,
+            self.priority, self.reserved, self.datapage, self.pduFormat, self.pduSpecific, self.sourceAddress
+        )
+
+    __repr__ = __str__
