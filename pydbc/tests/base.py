330647f36ac0baa2a43a35a41faf3bc6dd6b1c4f
Christoph Schueler <cpu12.gems@googlemail.com>
2018-08-05 13:02:35 +0100

    Add base.py

---
+++ pydbc/tests/base.py
@@ -0,0 +1,15 @@
+
+import unittest
+
+from pydbc.api.db import Database
+from pydbc.api.db import BaseObject
+
+
+class BaseTest(unittest.TestCase):
+
+  def setUp(self):
+      self.db = Database("test", inMemory = True)
+      
+  def tearDown(self):
+      self.db.close()
+            
