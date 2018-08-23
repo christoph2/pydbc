e045f3c1e0753885e2fa8151181c33ebcdb69394
Christoph Schueler <cpu12.gems@googlemail.com>
2018-08-23 15:40:22 +0200

    Add some more tests.

--- pydbc/tests/testmessages.py
+++ pydbc/tests/testmessages.py
@@ -8,10 +8,27 @@ from pydbc.api.db import DuplicateKeyError
 
 class TestMessage(BaseTest):
 
-  def testInsertMessageWorks(self):
-      msg = self.db.addMessage("MSG", 0x0815, 4, "test-message")
-      self.assertEqual(msg.name, "MSG")
-      self.assertEqual(msg.identifier, 0x0815)
-      self.assertEqual(msg.dlc, 4)
-      self.assertEqual(msg.comment, "test-message")
+    def createMessage(self):
+        return self.db.addMessage("MSG", 0x0815, 4, "test-message")
+
+    def testInsertMessageWorks(self):
+        msg = self.createMessage()
+        self.assertEqual(msg.name, "MSG")
+        self.assertEqual(msg.identifier, 0x0815)
+        self.assertEqual(msg.dlc, 4)
+        self.assertEqual(msg.comment, "test-message")
+
+    def testInsertDuplicateMessageFails(self):
+        msg1 = self.createMessage()
+        self.assertRaises(DuplicateKeyError, self.createMessage)
+
+    def testNewlyCreatedMessageHasNoSignals(self):
+        msg = self.createMessage()
+        signals = list(msg.signals())
+        self.assertEqual(signals, [])
+
+    def testAddSignalWorks(self):
+        msg = self.createMessage()
+        signal = msg.addSignal()
+
 
