82c70e6a0f1b9792252ee419ab0265ee57f21ba4
Christoph Schueler <cpu12.gems@googlemail.com>
2018-08-26 07:56:54 +0200

    Fix required Python version

--- setup.py
+++ setup.py
@@ -8,14 +8,15 @@ from glob import glob
 
 ANTLR_VERSION = '4.7.1'
 
+
 if os.environ.get("TRAVIS") or os.environ.get("APPVEYOR"):
     INSTALL_REQUIRES = ["antlr4-python3-runtime=={}".format(ANTLR_VERSION), 'mako', 'colorama', 'numpydoc', 'sphinxcontrib-napoleon']
 else:
     INSTALL_REQUIRES = ["antlr4-python3-runtime=={}".format(ANTLR_VERSION), 'mako', 'wxPython>=4.0.0', 'colorama', 'numpydoc', 'sphinxcontrib-napoleon']
 
 
-if (sys.version_info.major == 3 and sys.version_info.minor < 5) or (sys.version_info.minor < 3):
-    print("ERROR: pyDBC requires at least Python 3.5")
+if (sys.version_info.major == 3 and sys.version_info.minor < 4) or (sys.version_info.minor < 3):
+    print("ERROR: pyDBC requires at least Python 3.4")
     sys.exit(1)
 
 setup(
