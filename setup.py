08c979de72883d717192d2d1711f47741ab4571c
Christoph Schueler <cpu12.gems@googlemail.com>
2019-01-08 17:53:37 +0100

    Add license and classifiers

--- setup.py
+++ setup.py
@@ -39,6 +39,30 @@ setup(
     package_data = {
         "templates": glob('cgen/templates/*.tmpl'),
     },
-    test_suite="pydbc.tests"
+    test_suite="pydbc.tests",
+    license='GPLv2',
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+
 )
 
