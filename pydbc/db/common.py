bb741a72df485e4cbdf989c0d49c644b5bc9a514
Christoph Schueler <cpu12.gems@googlemail.com>
2019-10-14 11:52:02 +0200

    Add LinQueries

--- pydbc/db/common.py
+++ pydbc/db/common.py
@@ -29,11 +29,15 @@ __version__ = '0.1.0'
 
 import itertools
 
+from pydbc.logger import Logger
+from pydbc.types import AttributeType, ValueType, CategoryType
+
 class Queries:
     """
     """
 
     def __init__(self, db):
+        self.logger = Logger(__name__)
         self.db = db
 
     def getCursor(self):
@@ -310,3 +314,132 @@ class Queries:
         cur = self.getCursor()
         yield from self.db.fetchFromTable(cur, "Category_Value")
 
+
+class LinQueries(Queries):
+    """
+    """
+
+    def getAttributeDefinitionByName(self, name):
+        """
+        """
+        cur = self.getCursor()
+        result = list(self.db.fetchFromTable(cur, "linAttribute_Definition", where = "name = '{}'".format(name)))
+        if result:
+            return result[0]
+        else:
+            return {}
+
+    def getAttributeValues(self, name):
+        cur = self.getCursor()
+        attrDef = self.getAttributeDefinitionByName(name)
+        if attrDef:
+            rid = attrDef.get("RID")
+            return list(self.db.fetchFromTable(cur, "linAttribute_Value", where = "Attribute_Definition = {}".format(rid)))
+        else:
+            return []
+
+    def masterNode(self):
+        """Get basic information about master node.
+
+        Returns
+        -------
+            tuple
+                - node_name: str
+                - timebase: int
+                - jitter: int
+        """
+        cur = self.getCursor()
+        result = self.getAttributeValues("LIN_is_master")
+
+        if len(result) == 0:
+            self.logger.warn("No master node specified.")
+        elif len(result) > 1:
+            self.logger.warn("Multiple master nodes specified.")
+        else:
+            rid = result[0].get("Object_ID")
+            node = list(self.db.fetchFromTable(cur, "Node", where = "RID = {}".format(rid)))
+            name = node[0].get("Name")
+            tb = int(list(self.getAttributeValues("LIN_time_base"))[0].get("Num_Value"))
+            jt = int(list(self.getAttributeValues("LIN_jitter"))[0].get("Num_Value"))
+            return name, tb, jt
+
+    def slaveNodeNames(self):
+        """Get slave node names.
+
+        Returns
+        -------
+            list - Names of slave nodes.
+        """
+        cur = self.getCursor()
+        slaveIDs = [s['Object_ID'] for s in self.getAttributeValues("LIN_is_slave")]
+        cur.execute("SELECT Name from Node WHERE RID in ({}) ORDER BY RID".format(','.join([str(s) for s in slaveIDs])))
+        slaves =  [s[0] for s in cur.fetchall()]
+        return slaves
+
+    def frames(self):
+        """
+        """
+        cur = self.getCursor()
+        frames = list(self.db.fetchFromTable(cur, "Message", columns = "RID, Name, Message_ID, DLC, Sender", orderBy = "RID"))
+        for frame in frames:
+            mrid = frame['RID']
+            sender = self.fetchNodenameByRid(frame['Sender'])
+            frame.update(Sender = sender)
+            cur.execute("SELECT Signal, Offset FROM Message_Signal WHERE Message = {} ORDER BY 1".format(mrid))
+            rows = cur.fetchall()
+            messageSignals = []
+            for row in rows:
+                srid, offs = row
+                messageSignals.append((self.fetchSignalNameByRid(srid), offs))
+            frame.update(messageSignals = messageSignals)
+        return frames
+
+    def signals(self):
+        """
+        """
+        cur = self.getCursor()
+        signals = list(self.db.fetchFromTable(cur, "Signal", columns = "RID, Name, Bitsize", orderBy = "RID"))
+        iv = list(self.getAttributeValues("LIN_signal_initial_value"))
+        initialValues = dict([(x.get("Object_ID"), x.get("Num_Value")) for x in iv])
+        result = []
+        for signal in signals:
+            name = signal.get("Name")
+            rid = signal.get("RID")
+            bitsize = signal.get("Bitsize")
+            initialValue = initialValues[rid]
+            if isinstance(initialValue, str):
+                initialValue = [int(v) for v in initialValue.split(";")]
+            else:
+                initialValue = int(initialValue)
+            nrid = self.db.fetchSingleValue(cur, "Node_TxSig", "Node", "Signal = '{}'".format(rid))
+            cur.execute("SELECT name FROM node WHERE rid IN (SELECT node FROM Node_RxSignal WHERE signal = {})".format(rid))
+            subscribers = [s[0] for s in cur.fetchall()]
+            publisher = self.db.fetchSingleValue(cur, "Node", "Name", "RID = {}".format(nrid))
+            result.append((name, bitsize, initialValue, publisher, subscribers))
+        return result
+
+    def fetchSignalNameByRid(self, rid):
+        cur = self.getCursor()
+        return self.db.fetchSingleValue(cur, "Signal", "Name", "RID = {}".format(rid))
+
+
+    def networkAttributes(self):
+        """
+        """
+        cur = self.getCursor()
+        attrDefs = list(self.db.fetchFromTable(cur, "linAttribute_Definition", columns = "RID, Name, Valuetype",
+            orderBy = "RID", where = "Objecttype = {}".format(AttributeType.NETWORK)))
+        result = {}
+        for attrDef in attrDefs:
+            rid = attrDef.get("RID")
+            name = attrDef.get("Name")
+            value_type = attrDef.get("Valuetype")
+            cur.execute("SELECT Num_Value, String_Value FROM linAttribute_Value WHERE Attribute_Definition = {}".format(rid))
+            rvalue = cur.fetchone()
+            if rvalue:
+                num_value, string_value = rvalue
+                if num_value is None and string_value is None:
+                    continue
+                value = num_value if value_type in (ValueType.INT, ValueType.FLOAT) else string_value
+                result[name] = value
+        return result
