20e850d73d5cc82f80ebe86ff5cd47b7555d5cab
Christoph Schueler <cpu12.gems@googlemail.com>
2025-08-20 06:22:48 +0300

    Docs: update build

--- docs/conf.py
+++ docs/conf.py
@@ -41,8 +41,6 @@ release = '0.9'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
-#    'sphinx.ext.todo'
-    'sphinxcontrib.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -51,8 +49,7 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The master toctree document.
 master_doc = 'index'
@@ -62,7 +59,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
